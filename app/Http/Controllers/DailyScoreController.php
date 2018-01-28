43ff430e5ea2d8686d699b5302cbffdaabc78fc2
Juan Manuel Cruz <cruz.jm.stafe@gmail.com>
2018-01-28 05:58:34 -0300

    migracion de daily scores

---
+++ app/Http/Controllers/DailyScoreController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\DailyScore;
+use Illuminate\Http\Request;
+
+class DailyScoreController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\DailyScore  $dailyScore
+     * @return \Illuminate\Http\Response
+     */
+    public function show(DailyScore $dailyScore)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\DailyScore  $dailyScore
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(DailyScore $dailyScore)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\DailyScore  $dailyScore
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, DailyScore $dailyScore)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\DailyScore  $dailyScore
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(DailyScore $dailyScore)
+    {
+        //
+    }
+}
