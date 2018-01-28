4c89b3499557853b0f411628579ad5b77845b3c5
Juan Manuel Cruz <cruz.jm.stafe@gmail.com>
2018-01-28 06:05:28 -0300

    Armados los modelos

--- app/Influencer.php
+++ app/Influencer.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Influencer extends Model
 {
-    //
+    protected $fillable = [ 'name', 'player', 'score' ];
+    
+    protected $dates = [ 'created_at', 'updated_at' ];
 }
--- app/Score.php
+++ app/Score.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Score extends Model
 {
-    //
+    protected $fillable = [ 'score', 'position', 'player', 'influencer_id' ];
+
+    protected $dates = [ 'created_at', 'updated_at' ];
 }
