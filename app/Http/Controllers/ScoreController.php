ffd05e8f942bbb6d23f11d4a5a3747d8a10a8fe0
Juan Manuel Cruz <cruz.jm.stafe@gmail.com>
2018-02-28 02:28:21 -0300

    Encontramos el problema

--- app/Http/Controllers/ScoreController.php
+++ app/Http/Controllers/ScoreController.php
@@ -29,10 +29,8 @@ class ScoreController extends Controller
      */
     public function store(Request $request)
     {
-        dd($request);
-        $elems = Score::orderBy('score', 'desc')->get();
         $scores = $this->agregar_y_ordenar(
-            $elems,
+            Score::orderBy('score', 'desc')->get(),
             $request
         );
 
@@ -80,7 +78,7 @@ class ScoreController extends Controller
 
     private function updateDailysPosition()
     {
-        $dailys = DailyScore::orderBy('score'. 'desc')->get();
+        $dailys = DailyScore::orderBy('score', 'desc')->get();
         $dailys->each(function ($x, $key) { $x->position = $key; $x->save(); });
         if ($dailys->count() > 20) {
             $dailys = $dailys->sortByDesc('score');
@@ -90,28 +88,25 @@ class ScoreController extends Controller
 
     private function agregar_y_ordenar(Collection $elements, Request $request)
     {
-        dd($elements);
-
         $newScore = new Score($request->all());
         $newScore->position = 0;
-
-        dump($newScore);
 
         $elements->add($newScore);
         $elements = $elements->sortByDesc('score');
 
-        dump($elements);
-
         $buenos = $elements->slice(0, 20);
-
-        dump($buenos);
-
         $buenos = $buenos->each(function ($x, $key) {
             $x->position = $key;
             $x->save();
         });
 
-        $malos = $elements->slice(20)->pluck('id');
+        $malos = $elements
+            ->slice(20)
+            ->pluck('id')
+            ->filter(function ($x) {
+                return $x !== NULL;
+            });
+
         if (count($malos)) Score::destroy($malos);
 
         return array_values($buenos->toArray());
